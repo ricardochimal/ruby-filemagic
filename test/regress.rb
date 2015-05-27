1b303b1f74f7bc59555820817f9b60611a86ebf2
545ch4 <s@rprojekt.org>
2015-05-27 09:18:28 +0200

    Add version method
    Added version() instance method to get the version of the used libmagic.

--- test/regress.rb
+++ test/regress.rb
@@ -70,4 +70,10 @@ class TestFileMagic < Test::Unit::TestCase
     fm.flags = FileMagic::MAGIC_NONE
     assert_equal(FileMagic::MAGIC_NONE, fm.flags)
   end
+
+  def test_version
+    fm = FileMagic.new(FileMagic::MAGIC_SYMLINK | FileMagic::MAGIC_MIME)
+    fm_version = fm.version
+    assert_not_nil(fm_version)
+  end
 end
